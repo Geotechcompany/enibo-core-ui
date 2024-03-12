bd650632dd39a211f1f107b80ab9e9458cfcfab7
GeotechCompany <geoffreyaudia9@gmail.com>
2024-03-12 15:54:18 +0300

    modified:   src/store/branchType.tsx

--- src/store/branchType.tsx
+++ src/store/branchType.tsx
@@ -1,44 +1,63 @@
-import { Dispatch, FunctionComponent, ReactNode, SetStateAction, createContext, useContext, useState } from "react";
-
-
-interface BranchState {
+import {
+    Dispatch,
+    FunctionComponent,
+    ReactNode,
+    SetStateAction,
+    createContext,
+    useContext,
+    useEffect,
+    useState,
+  } from "react";
+  
+  interface BranchState {
     mode?: "ADD" | "COPY" | "EDIT"; 
     branchTypeName: string;
     description: string;
-}
-
-interface BranchProviderProps {
-    children?: ReactNode
-}
-
-interface BranchContextType {
+  }
+  
+  interface BranchProviderProps {
+    children?: ReactNode;
+  }
+  
+  interface BranchContextType {
     state: BranchState | undefined;
     setState: Dispatch<SetStateAction<BranchState | undefined>>;
-}
- 
-export const BranchStateContext = createContext<BranchContextType | undefined>(undefined)
-
-const BranchTypeProvider: FunctionComponent<BranchProviderProps> = ({children}: BranchProviderProps) => {
-    const [state, setState] = useState<BranchState | undefined >(undefined)
+  }
+   
+  export const BranchStateContext = createContext<BranchContextType | undefined>(undefined);
+  
+  const BranchTypeProvider: FunctionComponent<BranchProviderProps> = ({ children }: BranchProviderProps) => {
+    const [state, setState] = useState<BranchState | undefined>(() => {
+      const storedState = localStorage.getItem("branchTypeState");
+      // Check if the retrieved value is "undefined"
+      return storedState && storedState !== "undefined" ? JSON.parse(storedState) : undefined;
+    });
+  
+    useEffect(() => {
+      localStorage.setItem("branchTypeState", JSON.stringify(state));
+    }, [state]);
+  
     const value: BranchContextType = {
-        state,
-        setState,
-    }
-    return (  
-        <BranchStateContext.Provider value={value}>
+      state,
+      setState,
+    };
+  
+    return (
+      <BranchStateContext.Provider value={value}>
         {children}
-        </BranchStateContext.Provider>
+      </BranchStateContext.Provider>
     );
-}
- 
-export default BranchTypeProvider;
-
-export const useBranchTypeState = () => {
-    const context = useContext(BranchStateContext)
-
-    if(!context) {
-        throw new Error("")
+  };
+   
+  export default BranchTypeProvider;
+  
+  export const useBranchTypeState = () => {
+    const context = useContext(BranchStateContext);
+  
+    if (!context) {
+      throw new Error("useBranchTypeState must be used within a BranchTypeProvider");
     }
-
-    return context
-}
+  
+    return context;
+  };
+  
