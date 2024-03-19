de1e3d188187a80415ad512fdc4ec84bb058717b
GeotechCompany <geoffreyaudia9@gmail.com>
2024-03-19 15:42:16 +0300

    Transaction types

--- src/components/new-transaction-type.tsx
+++ src/components/new-transaction-type.tsx
@@ -27,7 +27,7 @@ const transactionTypeSchema = z.object({
     .string()
     .min(3, { message: "Transaction Type Code is required" }),
   description: z.string().min(3, { message: "Description is required" }),
-  currency: z.string().min(3, { message: "Currency is required" }),
+  currency: z.string().optional(),
   modifiedBy: z.string().optional(),
   modifiedOn: z.string().optional(),
 });
@@ -258,7 +258,7 @@ const NewTransactionTypeForm: FC<NewTransactionTypeFormProps> = () => {
             <Label htmlFor="transactionTypeCode">Transaction Type Code</Label>
             <Input
               id="transactionTypeCode"
-              type="number"
+              type="text"
               {...register("transactionTypeCode", { required: true })}
             />
             {errors.transactionTypeCode && (
